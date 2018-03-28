caef875992bb543a06c310af4df0a675c47b0b07
Terrance Boon <Boon67@msn.com>
2018-03-28 11:25:15 -0500

    Asset created by Boon67

---
+++ adapters/bleAdapter/files/start.sh/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then 
+  	echo "---------Permissions Error---------"
+  	echo "STOPPING: Please run as root or sudo"
+  	echo "-----------------------------------"
+  exit
+fi
+
+SCRIPTDIR="${0%/*}"
+CONFIGFILENAME="adapterconfig.txt"
+source "$SCRIPTDIR/$CONFIGFILENAME"
+
+systemctl start $ADAPTERSERVICENAME
